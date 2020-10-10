74d0ab0d8fd36cc368d7df89cb17a64ac1f42d37
Calvin Noronha <33203243+CalvinNor@users.noreply.github.com>
2020-10-10 22:30:39 +0530

    :construction: Add DayNight Theme (#10)
    - Added values-night directory for Night theme
    - Added new colors for Light theme
    - Added theme toggle via Menu
    - Added Theme enum for ease of use
    - Replaced color hardcoding with colorPrimary, colorPrimaryReversed
    - Fixed details image aspect ratio
    - Updated accent color from new TMDB branding
    - Removed unused resources

---
+++ core/src/main/java/com/calvinnor/core/utils/Theme.kt
@@ -0,0 +1,14 @@
+package com.calvinnor.core.utils
+
+enum class Theme {
+
+    /**
+     * Dark palette with light text.
+     */
+    NIGHT,
+
+    /**
+     * Light palette with dark text.
+     */
+    LIGHT
+}
